955c62cbcbc16e849d7dd3c95b7bf9bc91d914c8
Didier Matos <Didiermat00smail.com>
2018-02-24 17:04:25 -0500

    Se añadió un proyecto apenas iniciado de Android el cual es muy básico pero se ira mejorando junto con Git

---
+++ app/src/main/java/com/helloworld/innovasoftdl/MainActivity.java
@@ -0,0 +1,24 @@
+package com.helloworld.innovasoftdl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+}
